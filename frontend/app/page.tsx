824902462c64351eac65d2309aa2c4e26a8bd988
aiyza-junaid <aiyza.junaid@gmail.com>
2024-07-08 11:04:44 +0500

    addingg

---
+++ frontend/app/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+
+const Home: React.FC = () => {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/hello')
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+};
+
+export default Home;
