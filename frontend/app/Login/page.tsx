f0697c0dc98a7d2b592d623a3e3d2f3eed74dbbe
tk1475 <ahmedabclr35@gmail.com>
2024-07-10 11:07:01 +0500

    Adding login page

--- frontend/app/Login/page.tsx
+++ frontend/app/Login/page.tsx
@@ -1,10 +1,44 @@
-import React from "react";
+// frontend/app/Login/page.tsx
 
-const Login = () =>{
+import React from 'react';
+import '../../styles/login.css'; // Adjust the path based on your folder structure
+import Link from 'next/link'; // Import Link from Next.js for client-side navigation
 
-    return (
-        <div>Login</div>
-    )
-}
+const Login: React.FC = () => {
+  return (
+    <div className="background">
+      
+         <Link href="/">
+        <div className="homeButton">Locospace</div>
+      </Link>
+      
+      <Link href="/">
+      <img src="Logo.png" alt="Logo" className="logo" />
+      </Link>
 
-export default Login
+      <div className="loginBox">
+        <h2 >Sign in</h2>
+        <form>
+          <div className="inputBox">
+            <input type="text" required />
+            <label>Email</label>
+          </div>
+          <div className="inputBox">
+            <input type="password" required />
+            <label>Password</label>
+          </div>
+          <button type="submit">Login</button>
+          <div className="accountPrompt">
+            <span>Don't have an account? </span>
+            <Link href="./Signup/page">
+              <div className="createAccountLink">Create new account</div>
+            </Link>
+          </div>
+        </form>
+      
+      </div>
+    </div>
+  );
+};
+
+export default Login;
