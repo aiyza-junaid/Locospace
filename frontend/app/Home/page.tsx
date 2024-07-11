7943bdcb1cfe66ae3cae8ace14444b9d14e615df
aiyza-junaid <aiyza.junaid@gmail.com>
2024-07-11 10:57:35 +0500

    woooooooo

---
+++ frontend/app/Home/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import HomeNavBar from '../../components/HomeNavBar';
+
+
+const HomePage: React.FC = () => {
+ 
+  return (
+    <div>
+      <HomeNavBar />
+      
+    </div>
+  );
+};
+
+export default HomePage;
