38980718337cd277f8d5eb8b679a0536efde0d3f
aiyza-junaid <aiyza.junaid@gmail.com>
2024-07-11 09:30:35 +0500

    connection ayyyyy

--- frontend/app/Login/page.tsx
+++ frontend/app/Login/page.tsx
@@ -1,42 +1,78 @@
 // frontend/app/Login/page.tsx
 'use client'
-
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/login.css'; // Adjust the path based on your folder structure
 import Link from 'next/link'; // Import Link from Next.js for client-side navigation
+import useAuth from '../../authStore';
+import {useRouter} from 'next/navigation';
+
 
 const Login: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const setToken = useAuth(state => state.setToken);
+  const router = useRouter()
+
+
+  const handleLogin = async () => {
+
+    
+    console.log(email,password)
+    try {
+      const response = await fetch('http://localhost:5000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const { token } = await response.json();
+        setToken(token);
+        router.push('/')
+      } else {
+        console.error('Login failed:', response.statusText);
+        
+      }
+    } catch (error) {
+      console.error('Login error:');
+    }
+  };
+
   return (
     <div className="background">
-      
-         <Link href="/">
+      <Link href="/">
         <div className="homeButton">Locospace</div>
       </Link>
       
       <Link href="/">
-      <img src="Logo.png" alt="Logo" className="logo" />
+        <img src="Logo.png" alt="Logo" className="logo" />
       </Link>
 
       <div className="loginBox">
-        <h2 >Sign in</h2>
-        <form>
-          <div className="inputBox">
-            <input type="text" required />
-            <label>Email</label>
-          </div>
-          <div className="inputBox">
-            <input type="password" required />
-            <label>Password</label>
-          </div>
-          <button type="submit">Login</button>
-          <div className="accountPrompt">
-            <span>Don't have an account? </span>
-            <Link href="/Signup">
-              <div className="createAccountLink">Create new account</div>
-            </Link>
-          </div>
-        </form>
-      
+        <h2>Sign in</h2> 
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            handleLogin();
+          }}>
+            <div className="inputBox">
+              <input type="text" required value={email} onChange={(e) => setEmail(e.target.value)} />
+              <label>Email</label>
+            </div>
+            <div className="inputBox">
+              <input type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+              <label>Password</label>
+            </div>
+            <button type="submit">Login</button> 
+            <div className="accountPrompt">
+              <span>Don't have an account? </span>
+              <Link href="/Signup">
+                <div className="createAccountLink">Create new account</div>
+              </Link>
+            </div>
+          </form>
+        
       </div>
     </div>
   );
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,14 +7,7 @@ import CardCarouselComp from '../components/LandingCardCarouselComp';
 import Footer from '../components/LandingFooter';
 
 const LandingPage: React.FC = () => {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:5000/api/hello')
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
+ 
   return (
     <div>
       <CustomNavBar />
