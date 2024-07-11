a815eaa5a0abef44e54e2c0073b0d3b3d428cd45
Abdullah Ahmad <abdullahclicks17@gmail.com>
2024-07-11 10:16:26 +0500

    Re edited the userRoutes.js file

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,8 @@ const User = require('../models/userModel');
 const express = require('express');
 const router = express.Router();
 import bcrypt from "bcrypt";
-export const signup = async (req, res) => {
+
+router.post ('/signup', async (req, res) => {
     const { username, password , fname ,lname ,email,phoneno , lat ,longi } = req.body;
     try {
       // HASH THE PASSWORD
@@ -36,5 +37,5 @@ export const signup = async (req, res) => {
       console.log(err);
       res.status(500).json({ message: "Failed to create user!" });
     }
-  };
+  });
 module.exports = router;
