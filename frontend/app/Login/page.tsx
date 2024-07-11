53326dd392982b20685a50da63e68979a18121e8
tk1475 <ahmedabclr35@gmail.com>
2024-07-11 09:41:09 +0500

    React icons + Login changes + Profile frontend

--- frontend/app/Login/page.tsx
+++ frontend/app/Login/page.tsx
@@ -4,6 +4,7 @@
 import React from 'react';
 import '../../styles/login.css'; // Adjust the path based on your folder structure
 import Link from 'next/link'; // Import Link from Next.js for client-side navigation
+import { IoEyeOff } from "react-icons/io5";
 
 const Login: React.FC = () => {
   return (
@@ -26,7 +27,10 @@ const Login: React.FC = () => {
           </div>
           <div className="inputBox">
             <input type="password" required />
-            <label>Password</label>
+            <label>
+              Password
+            </label>
+            <div className='Eye'>  <IoEyeOff style={{ verticalAlign: 'middle', marginLeft: '5px' }}></IoEyeOff> </div>
           </div>
           <button type="submit">Login</button>
           <div className="accountPrompt">
--- frontend/app/Signup/page.tsx
+++ frontend/app/Signup/page.tsx
@@ -1,10 +1,63 @@
-import React from "react";
+// frontend/app/Login/page.tsx
+'use client'
 
-const SignUp = () =>{
+import React from 'react';
+import '../../styles/signup.css'; // Adjust the path based on your folder structure
+import Link from 'next/link'; // Import Link from Next.js for client-side navigation
 
-    return (
-        <div>Sign Up</div>
-    )
-}
+const Login: React.FC = () => {
+  return (
+    <div className="background">
+      
+         <Link href="/">
+        <div className="homeButton">Locospace</div>
+      </Link>
+      
+      <Link href="/">
+      <img src="Logo.png" alt="Logo" className="logo" />
+      </Link>
 
-export default SignUp 
+      <div className="signupBox">
+        <h2 >Sign Up</h2>
+        <form>
+        <div className="formGrid"> 
+            <div className="inputBox">
+                <input type="email" required />
+                <label>Email</label>
+            </div>
+            <div className="inputBox">
+                <input type="password" pattern='[A-Z],[a-z],[0-9],{8-16}' required />
+                <label>Password</label>
+            </div>
+            <div className="inputBox">
+              <input type="password" pattern='[A-Z],[a-z],[0-9],{8-16}' required />
+              <label>Confirm Password</label>
+            </div>
+            <div className="inputBox">
+              <input type="tel" pattern="[0-9]{10}" required />
+              <label>Phone Number</label>
+            </div>
+            <div className="inputBox">
+              <input type="Name" pattern='[A-Z],[a-z],{8-16}' required />
+              <label>Name</label>
+            </div>
+            <div className="inputBox">
+              <input type="tel" pattern="[0-9]{10}" required />
+              <label>Phone Number</label>
+            </div>
+        </div>
+            <button type="submit">Sign Up</button>
+            <div className="loginPrompt">
+                <span>Already have an account? </span>
+                <Link href="/Login">
+                <div className="loginLink">Sign in</div>
+                </Link>
+            </div>
+        </form>
+      
+      </div>
+    </div>
+  );
+};
+
+export default Login;
