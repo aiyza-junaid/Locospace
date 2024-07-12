b6dacfcba7e21cb656741562c522f3ac41ee2b1e
Abdullah Ahmad <abdullahclicks17@gmail.com>
2024-07-12 12:46:16 +0500

    re worked on middleware.js

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server";
 export default function middleware(req ) {
     let verify = req.cookies.get("loggedIn");
     let url = req.url;
-    if(!verify && url.includes('/')){
-        return NextResponse.redirect('/login')
-    }
+    if(!verify && url.includes('/Profile')){
+         return NextResponse.redirect('/frontend/app/Login')
+     }
 }
 
